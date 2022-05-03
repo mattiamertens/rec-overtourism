6acfc2e28831a8f0684918d32239653513fc6725
El <mattianc@hotmail.it>
2022-05-03 16:23:36 +0100

    videos

---
+++ script.js
@@ -0,0 +1,45 @@
+// $('.place').on('mouseenter', function(){
+//     $(this).children('.left-info').children('.header').children('.destination-title').addClass('yellow-text');
+//     $(this).children('.left-info').children('.header').children('.square').addClass('on-screen');
+// }).on('mouseleave', function(){
+//     $(this).children('.left-info').children('.header').children('.destination-title').removeClass('yellow-text')
+//     $(this).children('.left-info').children('.header').children('.square').removeClass('on-screen');
+// })
+
+document.querySelector('.destinations').addEventListener('scroll', function(){
+
+    scrollDetection();
+});
+
+function scrollDetection() {
+    $('.place').each(function() {
+
+        var offset = this.getBoundingClientRect().top;
+        if (offset < 300 && offset > 120){
+            // console.log(this)
+        }
+    
+        if(offset < 530 && offset > 120){ 
+          $(this).find('.destination-title').addClass('yellow-text')
+          $(this).find('.square').addClass('on-screen');
+          $(this).data('')
+          var dataSource = this.getAttribute('data-attr')
+          console.log(dataSource)
+
+          $('.player').attr('src', dataSource)
+        }
+        
+        else{
+            $(this).find('.destination-title').removeClass('yellow-text')
+            $(this).find('.square').removeClass('on-screen');
+        }
+    })
+}
+
+window.onload = function() {
+var img = document.createElement("img");
+  img.setAttribute("src", "https://www.youtube.com/embed/K_Vg94nBiaY?controls=0");
+  img.setAttribute("class", "player");
+  img.setAttribute("alt", "Video");
+  document.getElementsByClassName("video-wrapper")[0].appendChild(img);
+}
