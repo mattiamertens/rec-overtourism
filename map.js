3a79b8a3ccc13a8088555e35020292c2c9b43172
El <mattianc@hotmail.it>
2022-05-25 00:51:00 +0100

    single stream looking niceeee

---
+++ map.js
@@ -0,0 +1,28 @@
+// MAP MARKERS
+const venice = new mapboxgl.Marker()
+    .setLngLat([12.347337, 45.436674])
+    .addTo(map),
+    boracay = new mapboxgl.Marker()
+    .setLngLat([121.92473979033042, 11.968946748452733])
+    .addTo(map),
+    dubrovnik = new mapboxgl.Marker()
+    .setLngLat([18.110759418529227, 42.64139850493931])
+    .addTo(map),
+    mayaB = new mapboxgl.Marker()
+    .setLngLat([98.76598119568365, 7.676724786925769])
+    .addTo(map),
+    cinqueT = new mapboxgl.Marker()
+    .setLngLat([9.729454129038485, 44.1073633521156])
+    .addTo(map),
+    santorini = new mapboxgl.Marker()
+    .setLngLat([25.462121592740505, 36.39382747441639])
+    .addTo(map),
+    machuP = new mapboxgl.Marker()
+    .setLngLat([-72.54480794468684, -13.162385365206783])
+    .addTo(map),
+    mallorca = new mapboxgl.Marker()
+    .setLngLat([3.0270760435441506, 39.72470785203198])
+    .addTo(map),
+    canyon = new mapboxgl.Marker()
+    .setLngLat([ -18.171860286912676, 63.77198381927736])
+    .addTo(map);
