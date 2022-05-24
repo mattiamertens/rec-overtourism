3e7788cb248d2c7a05f57ce0e188fc58109a989d
El <mattianc@hotmail.it>
2022-05-24 17:23:26 +0100

    map looking crisp

---
+++ main.js
@@ -0,0 +1,15 @@
+// DA RIATTIVARE --> messo in index.html
+document.querySelector('.destinations').addEventListener('scroll', function(){
+
+    scrollDetection();
+});
+
+
+window.onload = function() {
+    var img = document.createElement("iframe");
+      img.setAttribute("src", "http://195.31.128.34:80/cgi-bin/faststream.jpg?stream=half&fps=15&rand=COUNTER");
+      img.setAttribute("class", "player");
+      img.setAttribute("alt", "Video");
+      document.getElementsByClassName("video-wrapper")[0].appendChild(img);
+    }
+
