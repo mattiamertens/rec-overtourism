367a55c7828510013c49c7e5c101b1563c1cb7a9
El <mattianc@hotmail.it>
2022-06-08 15:13:27 +0100

    videos

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ document.querySelector('.destinations').addEventListener('scroll', function(){
 });
 
 
-window.onload = function() {
-    var img = document.createElement("iframe");
-      img.setAttribute("src", "http://195.31.128.34:80/cgi-bin/faststream.jpg?stream=half&fps=15&rand=COUNTER");
-      img.setAttribute("class", "player");
-      img.setAttribute("alt", "Video");
-      document.getElementsByClassName("video-wrapper")[0].appendChild(img);
-    }
+// window.onload = function() {
+//   var img = document.createElement("video");
+//     img.setAttribute("src", "assets/videos/venice.mp4");
+//     img.setAttribute("class", "player");
+//     img.setAttribute("alt", "Video");
+//     img.autoplay = true;
+//     document.getElementsByClassName("video-wrapper")[0].appendChild(img);
+
+// }
 
